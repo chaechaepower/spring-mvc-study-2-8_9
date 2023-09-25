3e04454fe946251ac2f5031d723350cf58ec3bc6
Chaeyoon Kim <jkcyforever@gmail.com>
2023-09-25 20:43:03 +0900

    feat 1

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -13,6 +13,7 @@ public class ServletExController {
 
     @GetMapping("/error-ex")
     public void errorEx(){
+
         throw new RuntimeException("예외 발생!");
     }
 
@@ -20,6 +21,11 @@ public class ServletExController {
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404,"404 오류!");
+    }
+
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400,"400 오류!");
     }
 
     @GetMapping("/error-500")
